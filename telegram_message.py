b0d3289aa58a858402f8be8d1da1d05489a5cf71
Lipa <ivanlipovtsev@gmail.com>
2023-05-17 23:32:35 +0300

    some changes

--- telegram_message.py
+++ telegram_message.py
@@ -1,22 +1,34 @@
 from appium import webdriver
 
 def send_message(desire_caps, socket, tel, mes):
-  
+    """
+    the function is intended for automated emulation of human actions
+    for sending messages to the messenger telegram
+
+    :param desire_caps: emulator parameters
+    :param socket: emulator`s socket
+    :param tel: recipient's phone number
+    :param mes: message text
+    :return: bool
+    """
     try:
-        wd = webdriver.Remote(socket, desired_caps)
+        wd = webdriver.Remote(socket, desire_caps)
         wd.implicitly_wait(2)
         wd.find_elements_by_class_name('android.widget.TextView')[1].click()
         wd.find_elements_by_accessibility_id('New Message')[0].click()
         wd.find_elements_by_accessibility_id('Create New Contact')[0].click()
-        elements_enter = wd.find_elements_by_class_name('android.widget.EditText')
+        elements_enter = \
+            wd.find_elements_by_class_name('android.widget.EditText')
         elements_enter[0].send_keys('1')
         wd.find_elements_by_class_name('android.widget.TextView')[1].click()
         wd.find_elements_by_accessibility_id('Search')[0].click()
-        wd.find_elements_by_class_name('android.widget.EditText')[0].send_keys('russia')
+        wd.find_elements_by_class_name('android.widget.EditText')[0].\
+            send_keys('russia')
         wd.find_elements_by_class_name('android.widget.TextView')[0].click()
         elements_enter[3].send_keys(tel)
         wd.find_elements_by_accessibility_id('Done')[0].click()
-        wd.find_elements_by_class_name('android.widget.EditText')[0].send_keys(mes)
+        wd.find_elements_by_class_name('android.widget.EditText')[0].\
+            send_keys(mes)
         wd.find_elements_by_accessibility_id('Send')[0].click()
         wd.back()
         wd.back()
@@ -34,9 +46,10 @@ def main():
         udid= '0.0.0.0:5557',
         unicodeKeyboard = True)
     socket = 'http://127.0.0.1:4723/wd/hub'
-    tel = '79999999999'
-    mes = 'hello'
-    send_message(desire_caps, socket, tel, mes)
+    tel = 'some_telephone'
+    mes = 'some_message'
+    send_message(desired_caps, socket, tel, mes)
+
 
 if __name__ == "__main__":
     main()
--- whatsapp_message.py
+++ whatsapp_message.py
@@ -1,17 +1,32 @@
 from appium import webdriver
 
 def send_message_wtsp(desire_caps, socket, tel, mes):
-    wd = webdriver.Remote(socket, desired_caps)
+    """
+    the function is intended for automated emulation of human actions
+    for sending messages to the messenger whatsapp
+
+    :param desire_caps: emulator parameters
+    :param socket: emulator`s socket
+    :param tel: recipient's phone number
+    :param mes: message text
+    :return: bool
+    """
+    wd = webdriver.Remote(socket, desire_caps)
     wd.implicitly_wait(2)
     wd.find_elements_by_class_name('android.widget.TextView')[2].click()
     try:
         wd.find_element_by_class_name('android.widget.ImageButton').click()
-        wd.find_elements_by_id('com.whatsapp:id/contactpicker_row_name')[1].click()
-        wd.find_elements_by_class_name('android.widget.EditText')[0].send_keys('1')
-        wd.find_elements_by_class_name('android.widget.EditText')[2].send_keys(tel)
-        wd.find_element_by_id('com.android.contacts:id/editor_menu_save_button').click()
+        wd.find_elements_by_id('com.whatsapp:id/contactpicker_row_name')[1]\
+            .click()
+        wd.find_elements_by_class_name('android.widget.EditText')[0]\
+            .send_keys('1')
+        wd.find_elements_by_class_name('android.widget.EditText')[2].\
+            send_keys(tel)
+        wd.find_element_by_id('com.android.contacts:id/editor_menu_save_button')\
+            .click()
         wd.back()
-        wd.find_elements_by_id('com.whatsapp:id/contactpicker_row_name')[2].click()
+        wd.find_elements_by_id('com.whatsapp:id/contactpicker_row_name')[2]\
+            .click()
         wd.find_element_by_id('com.whatsapp:id/entry').click()
         wd.find_element_by_id('com.whatsapp:id/entry').send_keys(mes)
         wd.find_element_by_id('com.whatsapp:id/send').click()
@@ -19,8 +34,10 @@ def send_message_wtsp(desire_caps, socket, tel, mes):
         wd.back()
         wd.back()
         wd.find_elements_by_class_name('android.widget.TextView')[3].click()
-        wd.find_elements_by_class_name('android.widget.RelativeLayout')[2].click()
-        wd.find_elements_by_id('com.google.android.dialer:id/contact_name')[1].click()
+        wd.find_elements_by_class_name('android.widget.RelativeLayout')[2]\
+            .click()
+        wd.find_elements_by_id('com.google.android.dialer:id/contact_name')[1]\
+            .click()
         wd.find_element_by_class_name('android.widget.ImageButton').click()
         wd.find_elements_by_id('android:id/title')[1].click()
         wd.find_element_by_id('android:id/button1').click()
@@ -42,7 +59,7 @@ def main():
     socket = 'http://127.0.0.1:4723/wd/hub'
     tel = '79999999999'
     mes = 'hello'
-    send_message_wtsp(desire_caps, socket, tel, mes)
+    send_message_wtsp(desired_caps, socket, tel, mes)
 
 if __name__ == "__main__":
     main()
